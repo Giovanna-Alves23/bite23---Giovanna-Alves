f4e6bd9f2fd492c0c8d6439926385d92058fd4aa
Giovanna-Alves23 <giovanna.barroso@sptech.school>
2026-05-25 19:55:23 -0300

    Excluindo arquivos do projeto anterior e adicionando título nos gráficos da dashboard

--- web-data-viz/app.js
+++ web-data-viz/app.js
@@ -19,11 +19,6 @@ var indexRouter = require("./src/routes/index");
 var usuariosRouter = require("./src/routes/usuarios");
 var restaurantesRouter = require("./src/routes/restaurantes");
 
-var avisosRouter = require("./src/routes/avisos");
-var medidasRouter = require("./src/routes/medidas");
-var aquariosRouter = require("./src/routes/aquarios");
-var empresasRouter = require("./src/routes/empresas");
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -33,11 +28,6 @@ app.use(cors());
 app.use("/", indexRouter);
 app.use("/usuarios", usuariosRouter);
 app.use("/restaurantes", restaurantesRouter);
-
-app.use("/avisos", avisosRouter);
-app.use("/medidas", medidasRouter);
-app.use("/aquarios", aquariosRouter);
-app.use("/empresas", empresasRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
